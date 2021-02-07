c4b6805e78303120f47b10bdf523ce9ec25b7f9f
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2021-02-07 20:03:31 +0530

    Add certificate

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,23 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const app = require('./app');
 const config = require('./config');
 
 const PORT = config.port || 9999;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Listening on port http://localhost:${9999}`);
+// });
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+  },
+  app
+);
+
+sslServer.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${9999}`);
 });
